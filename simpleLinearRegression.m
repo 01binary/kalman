5b968c92c47e85c06a5e8eec2319f774c6689a5b
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-09-02 12:00:31 -0700

    Simplify

--- simpleLinearRegression.m
+++ simpleLinearRegression.m
@@ -1,17 +1,18 @@
-function output = linearRegressionFilter(xInput, yInput, damping)
-  output = zeros(length(xInput), 1);
-  errors = zeros(length(xInput), 1);
+function output = simpleLinearRegression(inputs, outputs, learningRate)
+  output = zeros(length(inputs), 1);
+  errors = zeros(length(inputs), 1);
   weight = 0;
 
-  for n = 1:length(xInput)
-    x = xInput(n);
-    y = yInput(n);
+  for n = 1:length(inputs)
+    x = inputs(n);
+    y = outputs(n);
 
     errors(n) = y - weight * x;
-    totalSquaredError = sum(errors)^2 / length(xInput);
-    weight = weight - totalSquaredError * damping;
+    totalSquaredError = sum(errors)^2 / length(inputs);
+    weight = weight - totalSquaredError * learningRate;
+    prediction = weight * x;
 
-    output(n) = weight * x;
+    output(n) = prediction;
   end
 end
  
